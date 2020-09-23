bb86e50fe64fe6105bf9e62b7168b9bc22391e3c
rosemary.ogbonna <rosemary.ogbonna@turnkeyafrica.com>
2020-09-23 22:27:08 +0100

    week 1 day 2 class assignment 1

---
+++ week_1_day_2/class_assignment1.py
@@ -0,0 +1,9 @@
+# Program to compute average of 4 numbers
+average = 87 + 43 + 71 + 60 / 4
+print(average)
+
+# Program to compute 10 percent of a cost
+
+cost = 500
+result = 10/100 * cost
+print(result)
